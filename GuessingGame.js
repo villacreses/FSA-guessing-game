2092307604b21c5eef4635922497bd3f1860a62b
Mario Villacreses <MarioVillacreses@outlook.com>
2018-03-10 23:43:46 -0500

    Added jquery functionality

--- GuessingGame.js
+++ GuessingGame.js
@@ -16,7 +16,6 @@ function shuffle (arr) {
     return arr;
 }
 
-
 function Game () {
     this.playersGuess = null;
     this.pastGuesses = [];
@@ -31,34 +30,44 @@ Game.prototype.isLower = function () {
     return (this.playersGuess < this.winningNumber);
 }
 
-Game.prototype.playersGuessSubmission = function (num) {
-    if ((typeof num !== 'number') || num < 1 || num > 100) {
-        throw 'That is an invalid guess.';
+Game.prototype.playersGuessSubmission = function () {
+    if ((typeof this.playersGuess !== 'number') || 
+        Number.isNaN(this.playersGuess) || 
+        this.playersGuess < 1 || this.playersGuess > 100) {
+      throw 'That is an invalid guess.';
     } else {
-        this.playersGuess = num;
-        return this.checkGuess();
+      return this.checkGuess();
     }
 }
 
-Game.prototype.checkGuess = function () {
-    if(this.playersGuess === this.winningNumber)
-        return "You Win!";
-    else if(this.pastGuesses.includes(this.playersGuess))
-        return "You have already guessed that number.";
-    else { 
-        this.pastGuesses.push(this.playersGuess);
+Game.prototype.correctAnswer = function (guess = this.playersGuess) {
+  return (guess === this.winningNumber);
+}
 
-        if(this.pastGuesses.length === 5)
-            return "You Lose.";
-        else if(this.difference() < 10)
-            return "You're burning up!";
-        else if(this.difference() < 25)
-            return "You're lukewarm.";
-        else if(this.difference() < 50)
-            return "You're a bit chilly.";
-        else 
-            return "You're ice cold!";
-    }
+Game.prototype.repeatedGuess = function () {
+  return this.pastGuesses.includes(this.playersGuess);
+}
+
+Game.prototype.checkGuess = function () {
+  if (this.repeatedGuess())
+    return "You have already guessed that number.";
+
+  this.pastGuesses.push(this.playersGuess);
+
+  let flavortext = this.isLower() ? ' Guess higher!' : ' Guess lower!';
+
+  if (this.correctAnswer())
+    return "You Win!";
+  else if(this.pastGuesses.length === 5)
+    return "You Lose.";
+  else if(this.difference() < 10)
+    return "You're burning up!" + flavortext;
+  else if(this.difference() < 25)
+    return "You're lukewarm." + flavortext;
+  else if(this.difference() < 50)
+    return "You're a bit chilly." + flavortext;
+  else 
+    return "You're ice cold!" + flavortext;
 }
 
 function newGame () {
@@ -74,3 +83,62 @@ Game.prototype.provideHint = function () {
 
     return arr;
 }
+
+$(document).ready(function () {
+  let game, subheading = $('h2');
+  
+  let resetGame = function () {
+    game = newGame(),
+    subheading.text('Guess a number between 1 and 100! (inclusive)');
+
+    $('.list-inline-item').each(function (i) {
+      $(this).text('-');
+      $(this).removeClass('.right .wrong');
+    });
+  }
+
+  resetGame();
+
+  let toggleInput = function (t) {
+    $('#submit').prop('disabled', t);
+    $('#player-input').prop('disabled', t);
+    $('#hint').prop('disabled', t);
+  }
+
+  let evaluateInput = function () {
+    game.playersGuess = Number.parseInt($('#player-input').val());
+    $('#player-input').val('');
+
+    subheading.text(game.playersGuessSubmission());
+    if(subheading.text() !== 'You have already guessed that number.') {
+      $('.list-inline-item').each(function (i) {
+        let input = game.pastGuesses[i] ? game.pastGuesses[i] : '-';
+        let setBG = game.correctAnswer() ? '.right':'.wrong';
+        $(this).text(input);
+      })
+    }
+
+    if (subheading.text() === 'You Win!') {
+      toggleInput(1);
+    } else if (subheading.text() === 'You Lose.') {
+      toggleInput(1);
+      subheading.text('You Lose. The answer was: ' + game.winningNumber);
+    }
+  }
+
+  $('#submit').click(evaluateInput);
+
+  $('input').on('keypress', function (event) {
+    if(event.which === 13) evaluateInput();
+  });
+
+  $('#hint').click(function () {
+    let hints = game.provideHint().join('  ');
+    subheading.text('One of these is the answer: ' + hints);
+  });
+
+  $('#reset').click(function () {
+    toggleInput(0);
+    resetGame();
+  });
+});
